540ff9fbce80af8bea95aaf89f6c4a0e7aca6aaf
bbrowning918 <benbrowning918@gmail.com>
2022-02-19 14:02:58 -0600

    start game handler

--- server/app.py
+++ server/app.py
@@ -1,13 +1,48 @@
 import asyncio
+import secrets
+import json
 
 import websockets
 
 import config
+from domain.model import Game, Power
+from services.qr import make_qr_code
+
+
+async def play(websocket, game, connected):
+    pass
+
+
+async def join(websocket, game):
+    pass
+
+
+async def start(websocket):
+    connected = {websocket}
+    host_key = secrets.token_urlsafe(6)
+    game_id = secrets.token_urlsafe(6)
+
+    game = Game(
+        id=game_id, host=host_key, clients={}, ipp={}, turn=0, power=Power.GERMANY
+    )
+
+    qr = make_qr_code(game_id)
+
+    try:
+        event = {"type": "init", "token": host_key, "game": game_id, "qr": qr}
+        await websocket.send(json.dumps(event))
+        await play(websocket, game, connected)
+    finally:
+        del game
 
 
 async def handler(websocket):
     async for message in websocket:
-        print(message)
+        event = json.loads(message)
+        print(event)
+
+        if event["type"] == "start":
+            await start(websocket)
 
 
 async def main():
