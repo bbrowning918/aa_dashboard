d4c108565a70f56978efbf2261f7055cc5564bfd
bbrowning918 <benbrowning918@gmail.com>
2022-02-20 17:56:22 -0600

    shuffle tests into own dirs

---
+++ client/src/state/__tests__/ipp.spec.ts
@@ -0,0 +1,288 @@
+import {
+    ippReducer,
+    nextTurn,
+    nextPower,
+    prevPower,
+    saveCurrent,
+} from "../ipp";
+import { Powers } from "../constants";
+
+describe("ippSlice tests", () => {
+    test("initialized state", () => {
+        expect(ippReducer(undefined, { type: undefined })).toEqual({
+            [Powers.GERMANY]: { 0: { start: 20 } },
+            [Powers.SOVIET_UNION]: { 0: { start: 8 } },
+            [Powers.COMMUNIST_CHINA]: { 0: { start: 2 } },
+            [Powers.JAPAN]: { 0: { start: 16 } },
+            [Powers.UK_WEST]: { 0: { start: 11 } },
+            [Powers.UK_EAST]: { 0: { start: 5 } },
+            [Powers.ANZAC]: { 0: { start: 3 } },
+            [Powers.FRANCE]: { 0: { start: 5 } },
+            [Powers.ITALY]: { 0: { start: 7 } },
+            [Powers.USA]: { 0: { start: 6 } },
+            [Powers.NATIONALIST_CHINA]: { 0: { start: 6 } },
+            ids: [0, 1],
+            currentId: 0,
+            currentPower: Powers.GERMANY,
+        });
+    });
+    test("nextTurn updates if all powers have saved", () => {
+        const state = {
+            [Powers.GERMANY]: {
+                0: { start: 20, spent: 20, income: 20 },
+                1: { start: 20 },
+            },
+            [Powers.SOVIET_UNION]: {
+                0: { start: 8, spent: 8, income: 8 },
+                1: { start: 8 },
+            },
+            [Powers.COMMUNIST_CHINA]: {
+                0: { start: 2, spent: 2, income: 2 },
+                1: { start: 2 },
+            },
+            [Powers.JAPAN]: {
+                0: { start: 16, spent: 16, income: 16 },
+                1: { start: 16 },
+            },
+            [Powers.UK_WEST]: {
+                0: { start: 11, spent: 11, income: 11 },
+                1: { start: 11 },
+            },
+            [Powers.UK_EAST]: {
+                0: { start: 5, spent: 5, income: 5 },
+                1: { start: 5 },
+            },
+            [Powers.ANZAC]: { 0: { start: 3, spent: 3, income: 3 }, 1: { start: 3 } },
+            [Powers.FRANCE]: {
+                0: { start: 5, spent: 5, income: 5 },
+                1: { start: 5 },
+            },
+            [Powers.ITALY]: { 0: { start: 7, spent: 7, income: 7 }, 1: { start: 7 } },
+            [Powers.USA]: { 0: { start: 6, spent: 6, income: 6 }, 1: { start: 6 } },
+            [Powers.NATIONALIST_CHINA]: {
+                0: { start: 6, spent: 6, income: 6 },
+                1: { start: 6 },
+            },
+            ids: [0, 1],
+            currentId: 0,
+            currentPower: Powers.NATIONALIST_CHINA,
+        };
+        expect(ippReducer(state, nextTurn())).toEqual({
+            [Powers.GERMANY]: {
+                0: { start: 20, spent: 20, income: 20 },
+                1: { start: 20 },
+            },
+            [Powers.SOVIET_UNION]: {
+                0: { start: 8, spent: 8, income: 8 },
+                1: { start: 8 },
+            },
+            [Powers.COMMUNIST_CHINA]: {
+                0: { start: 2, spent: 2, income: 2 },
+                1: { start: 2 },
+            },
+            [Powers.JAPAN]: {
+                0: { start: 16, spent: 16, income: 16 },
+                1: { start: 16 },
+            },
+            [Powers.UK_WEST]: {
+                0: { start: 11, spent: 11, income: 11 },
+                1: { start: 11 },
+            },
+            [Powers.UK_EAST]: {
+                0: { start: 5, spent: 5, income: 5 },
+                1: { start: 5 },
+            },
+            [Powers.ANZAC]: { 0: { start: 3, spent: 3, income: 3 }, 1: { start: 3 } },
+            [Powers.FRANCE]: {
+                0: { start: 5, spent: 5, income: 5 },
+                1: { start: 5 },
+            },
+            [Powers.ITALY]: { 0: { start: 7, spent: 7, income: 7 }, 1: { start: 7 } },
+            [Powers.USA]: { 0: { start: 6, spent: 6, income: 6 }, 1: { start: 6 } },
+            [Powers.NATIONALIST_CHINA]: {
+                0: { start: 6, spent: 6, income: 6 },
+                1: { start: 6 },
+            },
+            ids: [0, 1],
+            currentId: 1,
+            currentPower: Powers.GERMANY,
+        });
+    });
+    test("nextTurn no effect if not all powers have saved", () => {
+        const state = {
+            [Powers.GERMANY]: { 0: { start: 20 } },
+            [Powers.SOVIET_UNION]: { 0: { start: 8 } },
+            [Powers.COMMUNIST_CHINA]: { 0: { start: 2 } },
+            [Powers.JAPAN]: { 0: { start: 16 } },
+            [Powers.UK_WEST]: { 0: { start: 11 } },
+            [Powers.UK_EAST]: { 0: { start: 5 } },
+            [Powers.ANZAC]: { 0: { start: 3 } },
+            [Powers.FRANCE]: { 0: { start: 5 } },
+            [Powers.ITALY]: { 0: { start: 7 } },
+            [Powers.USA]: { 0: { start: 6 } },
+            [Powers.NATIONALIST_CHINA]: { 0: { start: 6 } },
+            ids: [0, 1],
+            currentId: 0,
+            currentPower: Powers.NATIONALIST_CHINA,
+        };
+        expect(ippReducer(state, nextTurn())).toEqual(state);
+    });
+    test("nextPower updates when not at end", () => {
+        const state = {
+            [Powers.GERMANY]: { 0: { start: 20 } },
+            [Powers.SOVIET_UNION]: { 0: { start: 8 } },
+            [Powers.COMMUNIST_CHINA]: { 0: { start: 2 } },
+            [Powers.JAPAN]: { 0: { start: 16 } },
+            [Powers.UK_WEST]: { 0: { start: 11 } },
+            [Powers.UK_EAST]: { 0: { start: 5 } },
+            [Powers.ANZAC]: { 0: { start: 3 } },
+            [Powers.FRANCE]: { 0: { start: 5 } },
+            [Powers.ITALY]: { 0: { start: 7 } },
+            [Powers.USA]: { 0: { start: 6 } },
+            [Powers.NATIONALIST_CHINA]: { 0: { start: 6 } },
+            ids: [0, 1],
+            currentId: 0,
+            currentPower: Powers.GERMANY,
+        };
+        expect(ippReducer(state, nextPower())).toEqual({
+            [Powers.GERMANY]: { 0: { start: 20 } },
+            [Powers.SOVIET_UNION]: { 0: { start: 8 } },
+            [Powers.COMMUNIST_CHINA]: { 0: { start: 2 } },
+            [Powers.JAPAN]: { 0: { start: 16 } },
+            [Powers.UK_WEST]: { 0: { start: 11 } },
+            [Powers.UK_EAST]: { 0: { start: 5 } },
+            [Powers.ANZAC]: { 0: { start: 3 } },
+            [Powers.FRANCE]: { 0: { start: 5 } },
+            [Powers.ITALY]: { 0: { start: 7 } },
+            [Powers.USA]: { 0: { start: 6 } },
+            [Powers.NATIONALIST_CHINA]: { 0: { start: 6 } },
+            ids: [0, 1],
+            currentId: 0,
+            currentPower: Powers.SOVIET_UNION,
+        });
+    });
+    test("nextPower no effect when at end", () => {
+        const state = {
+            [Powers.GERMANY]: { 0: { start: 20 } },
+            [Powers.SOVIET_UNION]: { 0: { start: 8 } },
+            [Powers.COMMUNIST_CHINA]: { 0: { start: 2 } },
+            [Powers.JAPAN]: { 0: { start: 16 } },
+            [Powers.UK_WEST]: { 0: { start: 11 } },
+            [Powers.UK_EAST]: { 0: { start: 5 } },
+            [Powers.ANZAC]: { 0: { start: 3 } },
+            [Powers.FRANCE]: { 0: { start: 5 } },
+            [Powers.ITALY]: { 0: { start: 7 } },
+            [Powers.USA]: { 0: { start: 6 } },
+            [Powers.NATIONALIST_CHINA]: { 0: { start: 6 } },
+            ids: [0, 1],
+            currentId: 0,
+            currentPower: Powers.NATIONALIST_CHINA,
+        };
+        expect(ippReducer(state, nextPower())).toEqual(state);
+    });
+    test("prevPower updates when not at start", () => {
+        const state = {
+            [Powers.GERMANY]: { 0: { start: 20 } },
+            [Powers.SOVIET_UNION]: { 0: { start: 8 } },
+            [Powers.COMMUNIST_CHINA]: { 0: { start: 2 } },
+            [Powers.JAPAN]: { 0: { start: 16 } },
+            [Powers.UK_WEST]: { 0: { start: 11 } },
+            [Powers.UK_EAST]: { 0: { start: 5 } },
+            [Powers.ANZAC]: { 0: { start: 3 } },
+            [Powers.FRANCE]: { 0: { start: 5 } },
+            [Powers.ITALY]: { 0: { start: 7 } },
+            [Powers.USA]: { 0: { start: 6 } },
+            [Powers.NATIONALIST_CHINA]: { 0: { start: 6 } },
+            ids: [0, 1],
+            currentId: 0,
+            currentPower: Powers.SOVIET_UNION,
+        };
+        expect(ippReducer(state, prevPower())).toEqual({
+            [Powers.GERMANY]: { 0: { start: 20 } },
+            [Powers.SOVIET_UNION]: { 0: { start: 8 } },
+            [Powers.COMMUNIST_CHINA]: { 0: { start: 2 } },
+            [Powers.JAPAN]: { 0: { start: 16 } },
+            [Powers.UK_WEST]: { 0: { start: 11 } },
+            [Powers.UK_EAST]: { 0: { start: 5 } },
+            [Powers.ANZAC]: { 0: { start: 3 } },
+            [Powers.FRANCE]: { 0: { start: 5 } },
+            [Powers.ITALY]: { 0: { start: 7 } },
+            [Powers.USA]: { 0: { start: 6 } },
+            [Powers.NATIONALIST_CHINA]: { 0: { start: 6 } },
+            ids: [0, 1],
+            currentId: 0,
+            currentPower: Powers.GERMANY,
+        });
+    });
+    test("prevPower no effect when at start", () => {
+        const state = {
+            [Powers.GERMANY]: { 0: { start: 20 } },
+            [Powers.SOVIET_UNION]: { 0: { start: 8 } },
+            [Powers.COMMUNIST_CHINA]: { 0: { start: 2 } },
+            [Powers.JAPAN]: { 0: { start: 16 } },
+            [Powers.UK_WEST]: { 0: { start: 11 } },
+            [Powers.UK_EAST]: { 0: { start: 5 } },
+            [Powers.ANZAC]: { 0: { start: 3 } },
+            [Powers.FRANCE]: { 0: { start: 5 } },
+            [Powers.ITALY]: { 0: { start: 7 } },
+            [Powers.USA]: { 0: { start: 6 } },
+            [Powers.NATIONALIST_CHINA]: { 0: { start: 6 } },
+            ids: [0, 1],
+            currentId: 0,
+            currentPower: Powers.GERMANY,
+        };
+        expect(ippReducer(state, prevPower())).toEqual({
+            [Powers.GERMANY]: { 0: { start: 20 } },
+            [Powers.SOVIET_UNION]: { 0: { start: 8 } },
+            [Powers.COMMUNIST_CHINA]: { 0: { start: 2 } },
+            [Powers.JAPAN]: { 0: { start: 16 } },
+            [Powers.UK_WEST]: { 0: { start: 11 } },
+            [Powers.UK_EAST]: { 0: { start: 5 } },
+            [Powers.ANZAC]: { 0: { start: 3 } },
+            [Powers.FRANCE]: { 0: { start: 5 } },
+            [Powers.ITALY]: { 0: { start: 7 } },
+            [Powers.USA]: { 0: { start: 6 } },
+            [Powers.NATIONALIST_CHINA]: { 0: { start: 6 } },
+            ids: [0, 1],
+            currentId: 0,
+            currentPower: Powers.GERMANY,
+        });
+    });
+    test("saveCurrent updates this turn and sets up next", () => {
+        const state = {
+            [Powers.GERMANY]: { 0: { start: 20 } },
+            [Powers.SOVIET_UNION]: { 0: { start: 8 } },
+            [Powers.COMMUNIST_CHINA]: { 0: { start: 2 } },
+            [Powers.JAPAN]: { 0: { start: 16 } },
+            [Powers.UK_WEST]: { 0: { start: 11 } },
+            [Powers.UK_EAST]: { 0: { start: 5 } },
+            [Powers.ANZAC]: { 0: { start: 3 } },
+            [Powers.FRANCE]: { 0: { start: 5 } },
+            [Powers.ITALY]: { 0: { start: 7 } },
+            [Powers.USA]: { 0: { start: 6 } },
+            [Powers.NATIONALIST_CHINA]: { 0: { start: 6 } },
+            ids: [0, 1],
+            currentId: 0,
+            currentPower: Powers.GERMANY,
+        };
+        expect(ippReducer(state, saveCurrent({ spent: 15, income: 20 }))).toEqual({
+            [Powers.GERMANY]: {
+                0: { start: 20, spent: 15, income: 20 },
+                1: { start: 25 },
+            },
+            [Powers.SOVIET_UNION]: { 0: { start: 8 } },
+            [Powers.COMMUNIST_CHINA]: { 0: { start: 2 } },
+            [Powers.JAPAN]: { 0: { start: 16 } },
+            [Powers.UK_WEST]: { 0: { start: 11 } },
+            [Powers.UK_EAST]: { 0: { start: 5 } },
+            [Powers.ANZAC]: { 0: { start: 3 } },
+            [Powers.FRANCE]: { 0: { start: 5 } },
+            [Powers.ITALY]: { 0: { start: 7 } },
+            [Powers.USA]: { 0: { start: 6 } },
+            [Powers.NATIONALIST_CHINA]: { 0: { start: 6 } },
+            ids: [0, 1],
+            currentId: 0,
+            currentPower: Powers.GERMANY,
+        });
+    });
+});
---
+++ client/src/state/ipp.ts
@@ -0,0 +1,104 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+import { RootState } from "./store";
+import { Powers, ORDER } from "./constants";
+import { Power } from "./types";
+
+type Turn = { start: number; spent?: number; income?: number };
+
+type IPPState = {
+    [Powers.GERMANY]: { [turnId: number]: Turn };
+    [Powers.SOVIET_UNION]: { [turnId: number]: Turn };
+    [Powers.COMMUNIST_CHINA]: { [turnId: number]: Turn };
+    [Powers.JAPAN]: { [turnId: number]: Turn };
+    [Powers.UK_WEST]: { [turnId: number]: Turn };
+    [Powers.UK_EAST]: { [turnId: number]: Turn };
+    [Powers.ANZAC]: { [turnId: number]: Turn };
+    [Powers.FRANCE]: { [turnId: number]: Turn };
+    [Powers.ITALY]: { [turnId: number]: Turn };
+    [Powers.USA]: { [turnId: number]: Turn };
+    [Powers.NATIONALIST_CHINA]: { [turnId: number]: Turn };
+    ids: Array<number>;
+    currentId: number;
+    currentPower: Power;
+};
+
+const initialState: IPPState = {
+    [Powers.GERMANY]: { 0: { start: 20 } },
+    [Powers.SOVIET_UNION]: { 0: { start: 8 } },
+    [Powers.COMMUNIST_CHINA]: { 0: { start: 2 } },
+    [Powers.JAPAN]: { 0: { start: 16 } },
+    [Powers.UK_WEST]: { 0: { start: 11 } },
+    [Powers.UK_EAST]: { 0: { start: 5 } },
+    [Powers.ANZAC]: { 0: { start: 3 } },
+    [Powers.FRANCE]: { 0: { start: 5 } },
+    [Powers.ITALY]: { 0: { start: 7 } },
+    [Powers.USA]: { 0: { start: 6 } },
+    [Powers.NATIONALIST_CHINA]: { 0: { start: 6 } },
+    ids: [0, 1],
+    currentId: 0,
+    currentPower: ORDER[0],
+};
+
+const ippSlice = createSlice({
+    name: "ipp",
+    initialState,
+    reducers: {
+        nextTurn: (state: IPPState) => {
+            const countPowersSubmitted = ORDER.reduce(
+                (count: number, power: Power) => {
+                    return state[power][state.currentId + 1] !== undefined
+                        ? count + 1
+                        : count;
+                },
+                0
+            );
+            if (countPowersSubmitted === ORDER.length) {
+                state.currentId += 1;
+                state.currentPower = ORDER[0];
+            }
+        },
+        nextPower: (state: IPPState) => {
+            const currentIndex = ORDER.findIndex(
+                (country) => country === state.currentPower
+            );
+            if (currentIndex + 1 < ORDER.length) {
+                state.currentPower = ORDER[currentIndex + 1];
+                state.ids = Array.from(new Set([...state.ids, state.currentId + 1]));
+            }
+        },
+        prevPower: (state: IPPState) => {
+            const currentIndex = ORDER.findIndex(
+                (country) => country === state.currentPower
+            );
+            if (currentIndex > 0) {
+                state.currentPower = ORDER[currentIndex - 1];
+            }
+        },
+        saveCurrent: (
+            state: IPPState,
+            action: PayloadAction<{ spent: number; income: number }>
+        ) => {
+            const { spent, income } = action.payload;
+
+            const start = state[state.currentPower][state.currentId].start;
+            state[state.currentPower][state.currentId].spent = spent;
+            state[state.currentPower][state.currentId].income = income;
+            state[state.currentPower][state.currentId + 1] = {
+                start: start - spent + income,
+            };
+        },
+    },
+});
+
+export const selectTurnIds = (state: RootState) => state.ipp.ids;
+export const selectTurnsForPower = (state: RootState, power: Power) =>
+    state.ipp[power];
+
+export const selectCurrentTurnId = (state: RootState) => state.ipp.currentId;
+export const selectCurrentTurn = (state: RootState) => state.ipp[state.ipp.currentPower][state.ipp.currentId];
+export const selectCurrentPower = (state: RootState) => state.ipp.currentPower;
+
+export const { nextTurn, nextPower, prevPower, saveCurrent } = ippSlice.actions;
+
+export const ippReducer = ippSlice.reducer;
