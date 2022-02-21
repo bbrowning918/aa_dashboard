f777fd957cf4e5772db8bb3f00286c7b352b6825
bbrowning918 <benbrowning918@gmail.com>
2022-02-20 22:26:14 -0600

    watch and join and play handlers

--- server/app.py
+++ server/app.py
@@ -5,47 +5,98 @@ import json
 import websockets
 
 import config
-from domain.model import Game, Power
+from domain.model import Game
 from services.qr import make_qr_code
+
+GAMES = {}
+
+
+async def error(websocket, message):
+    event = {
+        "type": "error",
+        "message": message,
+    }
+    await websocket.send(json.dumps(event))
 
 
 async def play(websocket, game, connected):
-    pass
+    async for message in websocket:
+        event = json.loads(message)
+        print(event)
+
+        event = {
+            "type": "update",
+            "ipp": [],
+        }
+        websockets.broadcast(connected, json.dumps(event))
 
 
-async def join(websocket, game):
-    pass
+async def watch(websocket, game_id):
+    try:
+        game, _ = GAMES[game_id]
+    except KeyError:
+        await error(websocket, "Game not found")
+        return
+    event = {
+        "type": "update",
+        "ipp": [],
+    }
+    await websocket.send(json.dumps(event))
+
+
+async def join(websocket, game_id):
+    try:
+        game, connected = GAMES[game_id]
+    except KeyError:
+        await error(websocket, "Game not found")
+        return
+
+    token = secrets.token_urlsafe(6)
+    connected.add(websocket)
+    # TODO include powers in event and assign token for access
+    try:
+        event = {
+            "type": "join",
+            "token": token,
+        }
+        await websocket.send(json.dumps(event))
+        await play(websocket, game, connected)
+    finally:
+        connected.remove(websocket)
 
 
 async def start(websocket):
-    connected = {websocket}
     token = secrets.token_urlsafe(6)
     game_id = secrets.token_urlsafe(6)
 
+    connected = {websocket}
     game = Game(id=game_id, host=token, clients={}, ipp={}, turn=0)
+
+    # TODO persist these
+    GAMES[game.id] = game, connected
 
     qr_code = make_qr_code(
         f"http://{config.get_http_hostname()}:{config.get_http_port()}/{game_id}/play"
     )
 
-    try:
-        event = {
-            "type": "init",
-            "payload": {"token": token, "game": game.id, "qr_code": qr_code},
-        }
-        await websocket.send(json.dumps(event))
-        await play(websocket, game, connected)
-    finally:
-        del game
+    event = {
+        "type": "init",
+        "payload": {"token": token, "game": game.id, "qr_code": qr_code},
+    }
+    await websocket.send(json.dumps(event))
+    await watch(websocket, game.id)
 
 
 async def handler(websocket):
     async for message in websocket:
         event = json.loads(message)
         print(event)
-
         if event["type"] == "start":
             await start(websocket)
+        elif event["type"] == "watch":
+            await watch(websocket, event["game"])
+        elif event["type"] == "join":
+            await join(websocket, event["game"])
 
 
 async def main():
