09ef3595d6a8348e8a823b4215d0149f22dc418c
bbrowning918 <benbrowning918@gmail.com>
2022-02-21 21:57:46 -0600

    game repository started

---
+++ server/adapters/repository.py
@@ -0,0 +1,60 @@
+import abc
+from ..domain.model import Game, Turn, IPP
+
+from tinydb import TinyDB
+from tinydb.table import Document
+
+
+class AbstractGameRepository(abc.ABC):
+    @abc.abstractmethod
+    def add(self, game: Game):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get(self, game_ref: str) -> Game:
+        raise NotImplementedError
+
+
+class TinyDBGameRepository(AbstractGameRepository):
+    def __init__(self, path):
+        self.db = TinyDB(path)
+
+    def add(self, game: Game):
+        document = {
+            "ref": game.ref,
+            "host": game.host,
+            "clients": {},
+            "turns": [
+                {
+                    "year": turn.year,
+                    "season": turn.season.value,
+                    "power": turn.power.value,
+                    "start": turn.ipp.start,
+                    "spent": turn.ipp.spent,
+                    "income": turn.ipp.income,
+                }
+                for turn in game.turns
+            ],
+        }
+        self.db.insert(Document(document, doc_id=hash(game)))
+
+    def get(self, game_ref: str) -> Game:
+        document = self.db.get(doc_id=hash(game_ref))
+        return Game(
+            ref=document["ref"],
+            host=document["host"],
+            clients=document["clients"],
+            turns={
+                Turn(
+                    year=turn["year"],
+                    season=turn["season"],
+                    power=turn["power"],
+                    ipp=IPP(
+                        start=turn["start"],
+                        spent=turn["spent"],
+                        income=turn["income"],
+                    ),
+                )
+                for turn in document["turns"]
+            },
+        )
