2ac917d74195a3250e3c12631034157c16ad18e7
bbrowning918 <benbrowning918@gmail.com>
2023-01-18 21:05:37 -0600

    start game dark and light mode styling

--- client/src/pages/StartGame.tsx
+++ client/src/pages/StartGame.tsx
@@ -25,19 +25,21 @@ export const StartGame = () => {
     }, []);
 
     return (
-        <div className="container mx-auto py-24">
-            <h1 className="title-font mb-12 text-center text-2xl font-medium text-gray-900 sm:text-3xl">
-                Global War 1936
-            </h1>
-            <div className="mx-auto flex max-w-xs flex-col justify-center">
-                <button
-                    className="rounded border-0 bg-blue-700 py-2 text-lg text-white hover:bg-blue-800 focus:outline-none"
-                    onClick={() =>
-                        sendMessage({ type: "start", payload: null })
-                    }
-                >
-                    Start Game
-                </button>
+        <div className="bg-white dark:bg-gray-900">
+            <div className="mx-auto max-w-screen-xl py-8 px-4 sm:py-16 lg:px-6">
+                <div className="mx-auto h-screen max-w-screen-sm text-center">
+                    <h1 className="title-font mb-12 text-4xl font-extrabold leading-tight tracking-tight text-gray-900 dark:text-white">
+                        Global War 1936
+                    </h1>
+                    <button
+                        className="mr-2 mb-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        onClick={() =>
+                            sendMessage({ type: "start", payload: null })
+                        }
+                    >
+                        Start Game
+                    </button>
+                </div>
             </div>
         </div>
     );
