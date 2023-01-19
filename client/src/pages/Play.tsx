13a98bff6befe70a41421800fd691a0d0de2df6d
bbrowning918 <benbrowning918@gmail.com>
2023-01-18 20:29:15 -0600

    play form using more vanilla formik

--- client/src/pages/Play.tsx
+++ client/src/pages/Play.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useCallback } from "react";
 import { useParams } from "react-router-dom";
-import { FormikErrors, useFormik } from "formik";
+import { ErrorMessage, Field, Form, Formik, FormikErrors } from "formik";
 
 import { useAppSelector } from "../state/hooks";
 import { selectCurrentTurnId } from "../state/ipp";
@@ -33,9 +33,7 @@ export const Play = () => {
     const handler = useCallback((message: Message) => {
         switch (message.type) {
             case "join":
-                console.log(
-                    "we joined, save the token, now we should pick powers"
-                );
+                console.log("we joined, save the token, then pick powers");
                 break;
             case "update":
                 console.log("there was an update to the game state, save it");
@@ -51,51 +49,59 @@ export const Play = () => {
         income: 0,
     };
 
-    const formik = useFormik<TurnFormProps>({
-        initialValues: initialValues,
-        enableReinitialize: true,
-        validate: (values: TurnFormProps) => {
-            let errors: FormikErrors<TurnFormProps> = {};
-
-            if (values.spent < 0) {
-                errors.spent = "It does not work that way";
-            }
-            if (values.spent > initialValues.start) {
-                errors.spent = "Trying to spend more than IPP";
-            }
-            if (values.income < 0) {
-                errors.income = "It does not work that way";
-            }
-
-            return errors;
-        },
-        onSubmit: (values: TurnFormProps, { resetForm }) => {
-            // TODO fire update message, redux will change in the handler
-            resetForm();
-        },
-    });
-
     return (
         <div className="container mx-auto max-w-sm py-24">
             <div className="flex flex-col items-center">
-                <form onSubmit={formik.handleSubmit}>
-                    <h6>INSERT POWER</h6>
-                    <p>{findSeasonYearForTurnId(currentTurnId)}</p>
-                    <div className="mb-6 grid gap-6">
-                        <label>IPP</label>
-                        <input type="number"></input>
-                        <label>Spent</label>
-                        <input type="number"></input>
-                        <label>Income</label>
-                        <input type="number"></input>
-                    </div>
-                    <button
-                        className="rounded border-0 bg-blue-700 py-2 text-lg text-white hover:bg-blue-800 focus:outline-none"
-                        type="submit"
-                    >
-                        Save
-                    </button>
-                </form>
+                <Formik
+                    initialValues={initialValues}
+                    validate={(values: TurnFormProps) => {
+                        let errors: FormikErrors<TurnFormProps> = {};
+
+                        if (values.spent < 0) {
+                            errors.spent = "It does not work that way";
+                        }
+                        if (values.spent > initialValues.start) {
+                            errors.spent = "Trying to spend more than IPP";
+                        }
+                        if (values.income < 0) {
+                            errors.income = "It does not work that way";
+                        }
+
+                        return errors;
+                    }}
+                    onSubmit={(values: TurnFormProps, { resetForm }) => {
+                        // TODO fire update message, redux will change in the handler
+                        resetForm();
+                    }}
+                >
+                    <Form>
+                        <h6>CHOSEN POWER</h6>
+                        <p>{findSeasonYearForTurnId(currentTurnId)}</p>
+                        <div className="mb-6 grid gap-6">
+                            <label htmlFor="start">IPP</label>
+                            <Field
+                                type="number"
+                                id="start"
+                                name="start"
+                                disabled
+                            />
+
+                            <label htmlFor="spent">Spent</label>
+                            <Field type="number" id="spent" name="spent" />
+                            <ErrorMessage name={"spent"} />
+
+                            <label htmlFor="income">Income</label>
+                            <Field type="number" id="income" name="income" />
+                            <ErrorMessage name={"income"} />
+                        </div>
+                        <button
+                            className="rounded border-0 bg-blue-700 py-2 text-lg text-white hover:bg-blue-800 focus:outline-none"
+                            type="submit"
+                        >
+                            Save
+                        </button>
+                    </Form>
+                </Formik>
             </div>
         </div>
     );
