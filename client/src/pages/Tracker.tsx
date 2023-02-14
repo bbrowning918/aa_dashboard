87de4b7867bd03ed6655ec2baf8097119599e192
bbrowning918 <benbrowning918@gmail.com>
2023-02-13 20:55:56 -0600

    Tracker page survives refreshes now

--- client/src/pages/Tracker.tsx
+++ client/src/pages/Tracker.tsx
@@ -1,12 +1,63 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-import { selectQrCode } from "../state/game";
-import { useAppSelector } from "../state/hooks";
+import { useWebsocket } from "../Websocket";
 
 import { CountryTable } from "../components/CountryTable";
 
+import {
+    selectConnected,
+    selectQrCode,
+    selectToken,
+    setConnected,
+} from "../state/game";
+import { useAppDispatch, useAppSelector } from "../state/hooks";
+import { setTurns } from "../state/turn";
+import { InboundMessage } from "../state/types";
+
 export const Tracker = () => {
+    const dispatch = useAppDispatch();
+    const { gameId } = useParams();
+
+    const { send, addHandler, removeHandler } = useWebsocket();
+
     const qrCode = useAppSelector(selectQrCode);
+    const token = useAppSelector(selectToken);
+    const connected = useAppSelector(selectConnected);
+
+    useEffect(() => {
+        addHandler(handler);
+        return () => removeHandler(handler);
+    }, []);
+
+    const handler = (message: InboundMessage) => {
+        if (message.type === "connected") {
+            dispatch(setConnected(message.payload));
+            dispatch(setTurns(message.payload));
+        }
+        if (message.type === "update") {
+            dispatch(setTurns(message.payload));
+        }
+    };
+
+    useEffect(() => {
+        if (!connected) {
+            if (gameId) {
+                token
+                    ? send({
+                          type: "join",
+                          payload: {
+                              token: token,
+                              game_ref: gameId,
+                          },
+                      })
+                    : send({
+                          type: "join",
+                          payload: { game_ref: gameId },
+                      });
+            }
+        }
+    }, [connected]);
 
     return (
         <div className="h-screen bg-white dark:bg-gray-900">
