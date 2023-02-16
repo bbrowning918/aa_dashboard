3802943295ffcb486f8b08cfb609468fe28c24a8
bbrowning918 <benbrowning918@gmail.com>
2023-02-15 20:40:43 -0600

    createBrowserRouter migration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,26 +1,36 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "./index.css";
 
-import { App } from "./App";
 import { store } from "./state/store";
 import { WebsocketProvider } from "./Websocket";
 
 import { ErrorBoundary } from "./ErrorBoundary";
+import { NewGame } from "./pages/NewGame";
+import { JoinGame } from "./pages/JoinGame";
+import { Draft } from "./pages/Draft";
+import { Tracker } from "./pages/Tracker";
+
+export const routes = [
+    { path: "/", element: <NewGame /> },
+    { path: ":gameId/join", element: <JoinGame /> },
+    { path: ":gameId/draft", element: <Draft /> },
+    { path: ":gameId/tracker", element: <Tracker /> },
+];
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <ErrorBoundary>
-            <BrowserRouter>
-                <Provider store={store}>
-                    <WebsocketProvider>
-                        <App />
-                    </WebsocketProvider>
-                </Provider>
-            </BrowserRouter>
+            <Provider store={store}>
+                <WebsocketProvider>
+                    <RouterProvider router={router} />
+                </WebsocketProvider>
+            </Provider>
         </ErrorBoundary>
     </React.StrictMode>
 );
